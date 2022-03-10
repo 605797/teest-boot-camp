0de9484b646b3e07b9aa172477df825428027c10
Siddhant Thapa Chhetry <90285183+605797@users.noreply.github.com>
2022-03-09 18:02:14 -0600

    Initial commit: demo

---
+++ src/main/java/com/technova/teestbootcamp/TeestBootCampApplication.java
@@ -0,0 +1,13 @@
+package com.technova.teestbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeestBootCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeestBootCampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technova/teestbootcamp/TeestBootCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technova.teestbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeestBootCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
